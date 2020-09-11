487c2ccc9893dfaa3a218140bbbd8e4a552f6b9a
Jeisson Julian Lozano Ortiz <ing.jeissonlozano@hotmail.com>
2020-09-10 21:15:47 -0500

    CRUD Marca

--- SistemaInventario.AccesoDatos/Migrations/20200911012651_agregarCategoria.Designer.cs
+++ SistemaInventario.AccesoDatos/Migrations/20200911012651_agregarCategoria.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaInventario.AccesoDatos.Data;
 
 namespace SistemaInventario.AccesoDatos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200911012651_agregarCategoria")]
+    partial class agregarCategoria
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -262,26 +264,6 @@ namespace SistemaInventario.AccesoDatos.Migrations
                     b.HasKey("id");
 
                     b.ToTable("Categorias");
-                });
-
-            modelBuilder.Entity("SistemaInventario.Modelos.Marca", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<bool>("Estado")
-                        .HasColumnType("bit");
-
-                    b.Property<string>("Nombre")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(50)")
-                        .HasMaxLength(50);
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Marcas");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
---
+++ SistemaInventario.Modelos/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SistemaInventario.Modelos
+{
+    public class Categoria
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Nombre Categoria")]
+        public string Nombre { get; set; }
+        [Required]
+        public bool Estado { get; set; }
+    }
+}
